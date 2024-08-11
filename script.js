ce23a4ffa383cb0deb4ac86bc25032dc89476249
tahmidjihan <tahmidjihan321@gmail.com>
2024-08-11 21:17:01 +0600

    added some changes in html and made the input search functional

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const searchBtn = document.getElementById("searchBtn");
-const username = document.querySelector(".input");
-
-const user = "https://api.github.com/users/hiteshchoudhary";
-const xhr = new XMLHttpRequest();
-xhr.open("GET", user);
-xhr.onreadystatechange = function () {
-  //   console.log(this.responseText);
-  if (xhr.readyState === 4) {
-    const data = JSON.parse(this.responseText);
-    console.log(data.login);
-  }
-};
-xhr.send();
+const username = document.querySelector(".input-username");
 
 //search
+let usernameVal;
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault(e);
+  usernameVal = username.value;
+  //main-function-search- API
+  const user = `https://api.github.com/users/${usernameVal}`;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", user);
+  xhr.onreadystatechange = function () {
+    //   console.log(this.responseText);
+    if (xhr.readyState === 4) {
+      if (xhr.status === 404) {
+        console.log("user not found");
+      } else {
+        const data = JSON.parse(this.responseText);
+        console.log(data.login);
+      }
+    }
+  };
+  xhr.send();
 });
+
+//show
