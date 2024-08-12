7297832695a723180f55da1047233a8e2ef20d1d
tahmidjihan <tahmidjihan321@gmail.com>
2024-08-12 11:29:50 +0600

    made the whole project functional

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ searchBtn.addEventListener("click", (e) => {
       } else {
         const data = JSON.parse(this.responseText);
         console.log(data.login);
+        processData(data);
       }
     }
   };
@@ -25,3 +26,28 @@ searchBtn.addEventListener("click", (e) => {
 });
 
 //show
+const followers = document.querySelector("#info1");
+const following = document.querySelector("#info2");
+const userLocation = document.querySelector("#info3");
+const repos = document.querySelector("#info4");
+const idCreated = document.querySelector("#info5");
+const twitter = document.querySelector("#info6");
+const blog = document.querySelector("#info7");
+const usernameOutput = document.querySelector("#username");
+const nameOutput = document.querySelector("#name");
+const cardImg = document.querySelector(".card-img");
+const bio = document.querySelector(".bio");
+
+function processData(data) {
+  cardImg.src = data.avatar_url;
+  nameOutput.innerHTML = data.name;
+  usernameOutput.innerHTML = `<a href="${data.html_url}">${data.login}</a>`;
+  followers.innerHTML = data.followers;
+  following.innerHTML = data.following;
+  userLocation.innerHTML = data.location;
+  repos.innerHTML = data.public_repos;
+  idCreated.innerHTML = data.created_at;
+  twitter.innerHTML = `<a href="https://x.com/${data.twitter_username}">@${data.twitter_username}</a>`;
+  blog.innerHTML = `<a href="${data.blog}">Blog</a>`;
+  bio.innerHTML = data.bio;
+}
